e2e26a3fa8b127e909579d2155a73b419b8e2bee
soheil-vd <vatandoust.soheil@gmail.com>
2025-09-13 00:11:18 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'vatandoust-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './products.json',
+  './images/icon-192x192.png',
+  './images/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
